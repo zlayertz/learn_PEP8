5f238f689925f1e934d5c55501fbc3bf63906111
zlayertz <poppkoppaf@gmail.com>
2026-01-28 13:05:53 +0700

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,3 +1,4 @@
-def very_long_function_name_with_many_parameters(param1, param2, param3, param4, param5, param6):
+def very_long_function_name_with_many_parameters(
+        param1, param2, param3, param4, param5, param6):
     total = param1 + param2 + param3 + param4 + param5 + param6
     return total * 1.18
